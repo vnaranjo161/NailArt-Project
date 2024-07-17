96ad7029234457e2356875511c81576171069c20
jhainethv161 <126474850+vnaranjo161@users.noreply.github.com>
2024-07-17 10:09:03 -0500

    prueba

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -3,6 +3,7 @@ import AuthController from '../controllers/user/authController';
 import registerController from '../controllers/user/registerController'
 import changePasswordController from '../controllers/user/changePasswordController'
 import validateToken from '../middleware/validateToken';
+import prueba from '../controllers/prueba';
 import {validator, validatorParams} from "../middleware/registerValidator"
 
 
@@ -11,6 +12,7 @@ const router = express.Router();
 
 router.post('/auth', AuthController);
 router.post('/register', validatorParams, validator, registerController);
-router.put('/changePassword', validateToken, changePasswordController)
+router.put('/changePassword', validateToken, changePasswordController);
+router.get('/prueba', prueba);
 
 export default router;
